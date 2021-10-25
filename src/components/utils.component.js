b5d549cce80368fcae71c75c1a04a28745111cc2
angelortizv <angelo2898@gmail.com>
2021-10-24 18:41:27 -0600

    [ADD] get Staff and UserTypes

--- src/components/utils.component.js
+++ src/components/utils.component.js
@@ -52,5 +52,31 @@ const getScheduleDays = (req, res) => {
       });
 };
 
+// get staff team
+const getStaff = (req, res) => {
+    const databaseConnection = getConnection();
+    databaseConnection.collection("staff").find({}, { projection: { _id: 0, courseId: 0 } } ).limit(20)
+    .toArray(function(error, data) {
+        if (error) {
+            res.status(400).send('⛔️ An error occurred getting all users ... \n[Error]: ' + error);
+        } else {
+            res.status(200).send(data);
+        }
+      });
+};
 
-module.exports = { getCourses, getLabs, getScheduleData, getScheduleDays }
+// get user types
+const getUserTypes = (req, res) => {
+    const databaseConnection = getConnection();
+    databaseConnection.collection("userTypes").find({}, { projection: { _id: 0, courseId: 0 } } ).limit(20)
+    .toArray(function(error, data) {
+        if (error) {
+            res.status(400).send('⛔️ An error occurred getting all users ... \n[Error]: ' + error);
+        } else {
+            res.status(200).send(data);
+        }
+      });
+};
+
+
+module.exports = { getCourses, getLabs, getScheduleData, getScheduleDays, getStaff, getUserTypes }
--- src/routes/utils.routes.js
+++ src/routes/utils.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getCourses, getLabs, getScheduleData, getScheduleDays } = require('../components/utils.component');
+const { getCourses, getLabs, getScheduleData, getScheduleDays, getStaff, getUserTypes } = require('../components/utils.component');
 
 router.get('/courses', getCourses);
 
@@ -9,5 +9,9 @@ router.get('/labs', getLabs);
 router.get('/scheduleData', getScheduleData);
 
 router.get('/scheduleDays', getScheduleDays);
+
+router.get('/staff', getStaff);
+
+router.get('/userTypes', getUserTypes);
 
 module.exports = router;
