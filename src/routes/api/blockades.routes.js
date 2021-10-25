d839d14c683fc3b0ae455784fd2a5f0784a6fbc5
angelortizv <angelo2898@gmail.com>
2021-10-25 00:34:47 -0600

    [ADD] Create, Update and Delete Blockades

--- src/routes/api/blockades.routes.js
+++ src/routes/api/blockades.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 
-const { getAllBlockades, getSingleBlockade } = require('../../components/blockades.component');
+const { getAllBlockades, getSingleBlockade, createBlockade, updateBlockade, removeBlockade } = require('../../components/blockades.component');
 
 var jsonParser = bodyParser.json();
 
@@ -10,5 +10,10 @@ router.get('/all', getAllBlockades);
 
 router.get('/:blockadeId', getSingleBlockade);
 
+router.post('/', jsonParser, createBlockade);
+
+router.put('/:blockadeId', jsonParser, updateBlockade);
+
+router.delete('/:blockadeId', removeBlockade);
 
 module.exports = router;
