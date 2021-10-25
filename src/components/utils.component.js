46409a75281777439c105d2125fd6dc9b9327059
angelortizv <angelo2898@gmail.com>
2021-10-24 18:35:24 -0600

    [ADD] getScheduleDays at 'v1/utils/scheduleDays'

--- src/components/utils.component.js
+++ src/components/utils.component.js
@@ -39,5 +39,18 @@ const getScheduleData = (req, res) => {
       });
 };
 
+// get schedule days
+const getScheduleDays = (req, res) => {
+    const databaseConnection = getConnection();
+    databaseConnection.collection("scheduleDays").find({}, { projection: { _id: 0, courseId: 0 } } ).limit(20)
+    .toArray(function(error, data) {
+        if (error) {
+            res.status(400).send('⛔️ An error occurred getting all users ... \n[Error]: ' + error);
+        } else {
+            res.status(200).send(data);
+        }
+      });
+};
 
-module.exports = { getCourses, getLabs, getScheduleData }
+
+module.exports = { getCourses, getLabs, getScheduleData, getScheduleDays }
--- src/routes/utils.routes.js
+++ src/routes/utils.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getCourses, getLabs, getScheduleData } = require('../components/utils.component');
+const { getCourses, getLabs, getScheduleData, getScheduleDays } = require('../components/utils.component');
 
 router.get('/courses', getCourses);
 
 router.get('/labs', getLabs);
 
 router.get('/scheduleData', getScheduleData);
+
+router.get('/scheduleDays', getScheduleDays);
 
 module.exports = router;
